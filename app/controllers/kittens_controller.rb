473b9bcd9f5c0b2dba42c0376091fa6528235dd9
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-06 23:37:54 +0300

    index and show respond to json requests

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,10 +1,20 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
   end
 
   def show
     @kitty = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitty }
+    end
   end
 
   def new
