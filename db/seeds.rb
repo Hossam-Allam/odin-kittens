42185d33764294210090b15e35127bf4c05d3212
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-06 22:16:25 +0300

    seeded kitties and updated index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+[ [ "Berry", 10, 10, 10 ], [ "Michu", 7, 10, 7 ] ].each do |kitty|
+  Kitten.find_or_create_by!(name: kitty[0], age: kitty[1], cuteness: kitty[2], softness: kitty[3])
+end
