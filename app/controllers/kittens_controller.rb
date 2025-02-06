31f4c5a65d5eb714ccad098bbf1d9ca9d01b84ab
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-06 23:03:01 +0300

    updated instance variables for consistent naming

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -21,6 +21,23 @@ class KittensController < ApplicationController
     end
   end
 
+  def edit
+    @kitty = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitty = Kitten.find(params[:id])
+      if @kitty.update_attributes(kitten_params)
+        flash[:success] = "Kitten was successfully updated"
+        redirect_to @kitty
+      else
+        flash[:error] = "Something went wrong"
+        render :edit
+      end
+  end
+
+
+
   private
 
   def kitten_params
